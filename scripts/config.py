47fa3f6fd17a91cbd3eb7ec0d002f960e9e37335
Richard-Sti <richard.stiskalek@protonmail.com>
2025-11-23 23:28:59 +0100

    Add paths config

---
+++ scripts/config.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+# Path to the root directory of Manticore data
+data_root = Path("/Users/rstiskalek/Data/Manticore/N256")
+# Path to the directory where results should be stored
+results_root = Path("/Users/rstiskalek/Projects/FlowIntegrator/results")
