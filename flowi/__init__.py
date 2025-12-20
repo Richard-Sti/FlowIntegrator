5a13ecb2f1a702b1b1e725c6141fbdf6f5cef1b5
Richard-Sti <richard.stiskalek@protonmail.com>
2025-12-20 12:37:16 +0100

    Updatei mports

--- flowi/__init__.py
+++ flowi/__init__.py
@@ -33,3 +33,15 @@ from .utils import (  # noqa: F401
     radec_to_galactic,
     galactic_to_radec,
 )
+
+from .velocity_convergence import (  # noqa: F401
+    matter_power_spectrum_camb,
+    velocity_sigma_1d_squared_within_radius,
+    expected_velocity_within_radius,
+)
+
+from .linear_theory import (  # noqa: F401
+    delta_to_velocity,
+    growth_rate,
+    hubble_parameter,
+)
